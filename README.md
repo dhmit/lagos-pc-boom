6c32153aea150cb46f5842be6c0a38c62262c5b5
anastasia <anastasia.aizman@gmail.com>
2021-10-05 11:19:41 -0400

    update - add wagtail, remove all else

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Boilerplate app
 You can use this to start a new project
+
+python manage.py createsuperuser
+Username: admin
+Email address: admin@example.com
+Password: password
+Password (again):
---
+++ lagos/app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-59dgi3%u(o@+q73t)_po94kk)$oue%*r7a3b3!@w^6_i-!*izs'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
