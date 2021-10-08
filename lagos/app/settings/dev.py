59b18eecad8a1c26b3ec055d91dd6bf613463fb3
anastasia <anastasia.aizman@gmail.com>
2021-10-08 09:41:52 -0400

    add - homepage, blog, django settings

--- lagos/app/settings/dev.py
+++ lagos/app/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-59dgi3%u(o@+q73t)_po94kk)$oue%*r7a3b3!@w^6_i-!*izs'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- lagos/home/models.py
+++ lagos/home/models.py
@@ -1,7 +1,19 @@
-from django.db import models
-
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+# @register_snippet
+# class NavBar(models.Model):
+#     """
+#     This is the navigation bar model
+#     """
+#     name = models.CharField(max_length=255)
+#     # menu_items =
