544a0a5f75042badb11e1a8758bd445528359be4
keileenorthcutt <90278366+keileenorthcutt@users.noreply.github.com>
2021-10-12 19:20:03 -0400

    authur model actually added

--- lagos/blog/models.py
+++ lagos/blog/models.py
@@ -13,30 +13,25 @@ from wagtail.search import index
 # Create your models here.
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
-    author = RichTextField(blank=True) #
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
-    content_panels = Page.content_panels + [
-        FieldPanel('author', classname="full")
-    ]
-
 
 class BlogPage(Page):
     date = models.DateField("Post date")
-    author = models.CharField(max_length=30, null=True)
+    author = models.CharField(max_length=30)
     intro = models.CharField(max_length=250, null=True)
     body = RichTextField(blank=True)
 
     search_fields = Page.search_fields + [
-        index.SearchField('author'), #
+        index.SearchField('author'),
         index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
-        FieldPanel('author'), #
+        FieldPanel('author'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
     ]
