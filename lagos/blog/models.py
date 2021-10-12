d3f25dddcce0fe1d5d5829109d4ee937865ca84d
keileenorthcutt <90278366+keileenorthcutt@users.noreply.github.com>
2021-10-12 18:51:41 -0400

    author model added

--- lagos/blog/models.py
+++ lagos/blog/models.py
@@ -13,24 +13,30 @@ from wagtail.search import index
 # Create your models here.
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
-
+    author = RichTextField(blank=True) #
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+    content_panels = Page.content_panels + [
+        FieldPanel('author', classname="full")
     ]
 
 
 class BlogPage(Page):
     date = models.DateField("Post date")
+    author = models.CharField(max_length=30, null=True)
     intro = models.CharField(max_length=250, null=True)
     body = RichTextField(blank=True)
 
     search_fields = Page.search_fields + [
+        index.SearchField('author'), #
         index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
+        FieldPanel('author'), #
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
     ]
